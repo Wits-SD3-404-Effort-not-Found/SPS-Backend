6b40cbc4ddde1ffff7d8852f6b113d2016ef93ed
Brendan Griffiths <orwellian225@gmail.com>
2023-03-27 22:24:58 +0200

    add: config

--- src/endpoints.rs
+++ src/endpoints.rs
@@ -1,8 +1,18 @@
 #[cfg(test)] mod tests;
 
+use crate::SETTINGS;
+
+// Code Demo:
+//  for defining an index
+//  Accessing settings, its async but might need to switched to sync later
 #[get("/")]
-pub fn index() -> &'static str {
+pub async fn index() -> &'static str {
     log::info!("Connecting to API Index");
+    async {
+        let settings = SETTINGS.read().await;
+        log::info!("test {}", settings.get::<String>("test_value").unwrap());
+    }.await;
+
     "Wits Student Placement System API"
 }
 
--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,38 @@
 mod endpoints;
 
-#[macro_use] extern crate rocket;
+// Any errors croping up on the next 3 lines can just be ignored
+#[macro_use]
+extern crate rocket;
+extern crate lazy_static;
 
 use simple_logger::SimpleLogger;
 use systemd_journal_logger::{
     connected_to_journal,
     init_with_extra_fields
 };
+use config::Config;
+use lazy_static::lazy_static;
+use tokio::sync::RwLock;
+
+// Add all our settings to a global variable
+// RwLock - https://docs.rs/tokio/1.26.0/tokio/sync/struct.RwLock.html
+// lazy_static - https://docs.rs/lazy_static/latest/lazy_static/
+// config - https://docs.rs/config/0.13.3/config/
+lazy_static! {
+    static ref SETTINGS: RwLock<Config> = RwLock::new(
+        Config::builder()
+            .add_source(config::File::with_name("./config.toml")).build().unwrap()
+    );
+}
 
 #[launch]
 fn rocket() -> _ {
 
+    // If the systemd service is setup, write to the journal
     if connected_to_journal() {
         init_with_extra_fields(vec![("CARGO_VERSION", env!("CARGO_PKG_VERSION"))]).unwrap();
     } else {
+        // Else just use stdout to write logs
         SimpleLogger::new().init().unwrap()
     }
 
