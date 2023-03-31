d9b9974011b93615879b55bb7a798eaab506fbe5
Konstantinos <konstantinoshatzipanis@gmail.com>
2023-04-01 01:50:28 +0200

    fix bug

--- sql/user_access.sql
+++ sql/user_access.sql
@@ -2,7 +2,7 @@ CREATE USER 'azgeda'@'%' IDENTIFIED BY 'freetopGbrothers';
 CREATE USER 'orwellian'@'%' IDENTIFIED BY 'orcasarecoolanimals';
 CREATE USER 'sps_api'@'%' IDENTIFIED BY 'belugasaredopetoo';
 
-GRANT ALL PRIVILEGES ON database_name.* TO 'azgeda'@'%';
-GRANT ALL PRIVILEGES ON database_name.* TO 'orwellian'@'%';
+GRANT ALL PRIVILEGES ON Apollonius.* TO 'azgeda'@'%';
+GRANT ALL PRIVILEGES ON Apollonius.* TO 'orwellian'@'%';
 
-GRANT SELECT, INSERT, UPDATE ON database_name.* TO 'sps_api'@'%';
+GRANT SELECT, INSERT, UPDATE ON Apollonius.* TO 'sps_api'@'%';
