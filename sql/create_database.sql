b57ecf9b21053704ec8b7087e5c3c935f49b65e5
Konstantinos <konstantinoshatzipanis@gmail.com>
2023-04-07 13:34:20 +0200

    Add: OTP table + Fix duplicate/ambiguous id's

--- sql/create_database.sql
+++ sql/create_database.sql
@@ -3,7 +3,7 @@ USE Apollonius;
 
 
 CREATE TABLE tblAccount (
-  id INT PRIMARY KEY AUTO_INCREMENT,
+  account_id INT PRIMARY KEY AUTO_INCREMENT,
   email VARCHAR(255) NOT NULL,
   hashed_password VARCHAR(255) NOT NULL,
   username VARCHAR(255) NOT NULL,
@@ -12,72 +12,75 @@ CREATE TABLE tblAccount (
 );
 
 CREATE TABLE tblHospital (
-  id INT PRIMARY KEY AUTO_INCREMENT,
+  hospital_id INT PRIMARY KEY AUTO_INCREMENT,
   name VARCHAR(255) NOT NULL
 );
 
 CREATE TABLE tblDiscipline (
-  id INT PRIMARY KEY AUTO_INCREMENT,
+  discipline_id INT PRIMARY KEY AUTO_INCREMENT,
   name VARCHAR(255) NOT NULL
 );
 
 CREATE TABLE tblSessionToken (
-  id INT PRIMARY KEY AUTO_INCREMENT,
+  session_token_id INT PRIMARY KEY AUTO_INCREMENT,
   account_id INT NOT NULL,
   token VARCHAR(255) NOT NULL,
   expiry_date DATE NOT NULL,
-  FOREIGN KEY (account_id) REFERENCES tblAccount(id)
+  FOREIGN KEY (account_id) REFERENCES tblAccount(account_id)
 );
 
 CREATE TABLE tblNotes (
-  id INT PRIMARY KEY AUTO_INCREMENT,
+  notes_id INT PRIMARY KEY AUTO_INCREMENT,
   note_id INT NOT NULL,
   account_id INT NOT NULL,
   title VARCHAR(255) NOT NULL,
   content TEXT,
-  FOREIGN KEY (account_id) REFERENCES tblAccount(id)
+  FOREIGN KEY (account_id) REFERENCES tblAccount(account_id)
 );
 
 CREATE TABLE tblProtocol (
-  id INT PRIMARY KEY AUTO_INCREMENT,
-  protocol_id INT NOT NULL,
+  protocol_id INT PRIMARY KEY AUTO_INCREMENT,
   title VARCHAR(255) NOT NULL,
   content TEXT
 );
 
 CREATE TABLE tblLogbook (
-  id INT PRIMARY KEY AUTO_INCREMENT,
-  logbook_id INT NOT NULL,
+  logbook_id INT PRIMARY KEY AUTO_INCREMENT,
   account_id INT NOT NULL,
-  FOREIGN KEY (account_id) REFERENCES tblAccount(id)
+  FOREIGN KEY (account_id) REFERENCES tblAccount(account_id)
 );
 
 CREATE TABLE tblEvents (
-  id INT PRIMARY KEY AUTO_INCREMENT,
-  event_id INT NOT NULL,
+  event_id INT PRIMARY KEY AUTO_INCREMENT,
   account_id INT NOT NULL,
   start_date DATE NOT NULL,
   end_date DATE NOT NULL,
   name VARCHAR(255) NOT NULL,
   description TEXT,
-  FOREIGN KEY (account_id) REFERENCES tblAccount(id)
+  FOREIGN KEY (account_id) REFERENCES tblAccount(account_id)
 );
 
 CREATE TABLE tblRotation (
-  id INT PRIMARY KEY AUTO_INCREMENT,
-  rotation_id INT NOT NULL,
+  rotation_id INT PRIMARY KEY AUTO_INCREMENT,
   start_date DATE NOT NULL,
   end_date DATE NOT NULL,
   hospital_id INT NOT NULL,
   discipline_id INT NOT NULL,
-  FOREIGN KEY (hospital_id) REFERENCES tblHospital(id),
-  FOREIGN KEY (discipline_id) REFERENCES tblDiscipline(id)
+  FOREIGN KEY (hospital_id) REFERENCES tblHospital(hospital_id),
+  FOREIGN KEY (discipline_id) REFERENCES tblDiscipline(discipline_id)
 );
 
 CREATE TABLE tblAssignedRotations (
-  id INT PRIMARY KEY AUTO_INCREMENT,
+  assigned_rotation_id INT PRIMARY KEY AUTO_INCREMENT,
   rotation_id INT NOT NULL,
   account_id INT NOT NULL,
-  FOREIGN KEY (rotation_id) REFERENCES tblRotation(id),
-  FOREIGN KEY (account_id) REFERENCES tblAccount(id)
+  FOREIGN KEY (rotation_id) REFERENCES tblRotation(rotation_id),
+  FOREIGN KEY (account_id) REFERENCES tblAccount(account_id)
+);
+
+CREATE TABLE tblOTP(
+  otp_id INT PRIMARY KEY NOT NULL,
+  account_id INT NOT NULL,
+  opt_num VARCHAR(8) NOT NULL,
+  FOREIGN KEY (account_id) REFERENCES tblAccount(account_id)
 );
