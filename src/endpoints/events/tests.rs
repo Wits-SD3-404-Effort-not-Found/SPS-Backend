0bf3e3111b8b3baa2cae886533ecef6a7c749d62
Brendan Griffiths <orwellian225@gmail.com>
2023-05-14 16:02:28 +0200

    add: additional tests

--- src/endpoints/events/tests.rs
+++ src/endpoints/events/tests.rs
@@ -26,3 +26,35 @@ fn test_fetch_events_existing_account_without_events_not_found() {
     assert_eq!(response.status(), Status::NotFound);
     assert!(response.body().is_some());
 }
+
+#[test]
+fn test_insert_event_existing_account_ok() {
+    let client_binding = CLIENT.lock().unwrap();
+    let body = super::event_api::NewEvent {
+        account_id: 1,
+        start_date: "2023-01-01 00:00:00.000".to_string(),
+        end_date: "2023-01-01 00:00:00.000".to_string(),
+        event_name: "TEST ROCKET EVENT INSERT".to_string(),
+        description: Some("TEST ROCKET EVENT DESCRIPTION".to_string()),
+    };
+    let response = client_binding.post(uri!(super::add_event))
+        .body(serde_json::to_string(&body).unwrap()).dispatch();
+    assert_eq!(response.status(), Status::Ok);
+    assert!(response.body().is_none());
+}
+
+#[test]
+fn test_insert_event_nonexisting_account_not_found() {
+    let client_binding = CLIENT.lock().unwrap();
+    let body = super::event_api::NewEvent {
+        account_id: 0,
+        start_date: "2023-01-01 00:00:00.000".to_string(),
+        end_date: "2023-01-01 00:00:00.000".to_string(),
+        event_name: "TEST ROCKET EVENT INSERT".to_string(),
+        description: Some("TEST ROCKET EVENT DESCRIPTION".to_string()),
+    };
+    let response = client_binding.post(uri!(super::add_event))
+        .body(serde_json::to_string(&body).unwrap()).dispatch();
+    assert_eq!(response.status(), Status::NotFound);
+    assert!(response.body().is_some());
+}
